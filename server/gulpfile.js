e3260b6aba56dfd001b13d45b75eaa7686513abd
Daniel Kellenberger <daniel@kellenberger.li>
2016-09-14 22:09:35 +0200

    - try to have server part compiled with gulp

---
+++ server/gulpfile.js
@@ -0,0 +1,22 @@
+var gulp = require('gulp');
+var ts = require('gulp-typescript');
+var serverTS = ["src/**/*.ts", "typings/**/*.ts", "node_modules/**/*.d.ts"];
+
+
+var tsProject = ts.createProject("tsconfig.json", {
+    typescript: require('typescript')
+});
+
+gulp.task('default', ['ts', 'watch']);
+
+gulp.task('ts', function() {
+    return tsProject
+        .src(serverTS, {base: './'})
+        .pipe(ts(tsProject))
+        .js
+        .pipe(gulp.dest('./www'));
+});
+
+gulp.task('watch', function() {
+    gulp.watch('./src/**/*.ts', ['ts']);
+});
