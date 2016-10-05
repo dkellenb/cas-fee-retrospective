36533805c3cb843462c404048cc383be738acda3
bruede <benj.ruede@gmail.com>
2016-10-05 11:48:16 +0200

    Client - new Module core --Import Guard for core module

---
+++ retrospective-client/src/app/core/core.module.ts
@@ -0,0 +1,16 @@
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {CoreComponent} from './core.component';
+import {throwIfAlreadyLoaded} from "./module-import-guard";
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [CoreComponent]
+})
+export class CoreModule {
+  constructor(@Optional() @SkipSelf() parentModule:CoreModule) {
+    throwIfAlreadyLoaded(parentModule, 'CoreModule');
+  }
+}
