0c7151a1190635cd24a5a202b4ac92636d4085ec
bruede <benj.ruede@gmail.com>
2016-10-05 11:58:11 +0200

    Client - new component board

--- retrospective-client/src/app/app.module.ts
+++ retrospective-client/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { BoardComponent } from './shared/board/board.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BoardComponent
   ],
   imports: [
     BrowserModule,
---
+++ retrospective-client/src/app/shared/board/board.component.spec.ts
@@ -0,0 +1,11 @@
+/* tslint:disable:no-unused-variable */
+
+import { TestBed, async } from '@angular/core/testing';
+import { BoardComponent } from './board.component';
+
+describe('Component: Board', () => {
+  it('should create an instance', () => {
+    let component = new BoardComponent();
+    expect(component).toBeTruthy();
+  });
+});
