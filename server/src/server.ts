a6ded6673b2b9f61833da36f4ee44ee33a4e3658
Daniel Kellenberger <daniel@kellenberger.li>
2016-11-15 12:11:21 +0100

    - add config for ui port

--- server/src/server.ts
+++ server/src/server.ts
@@ -43,7 +43,7 @@ server.setConfig((app) => {
   app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:' + nconf.get('ui-port'));
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
