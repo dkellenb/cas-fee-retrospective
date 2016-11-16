77b2266c3966be4f6f2cf549e19e411f7cf7d4c8
bruede <benj.ruede@gmail.com>
2016-11-16 15:36:47 +0100

    Client: - createRetrospective implemented - getRetrospective start implementation

--- client/src/app/shared/services/retrospective.service.ts
+++ client/src/app/shared/services/retrospective.service.ts
@@ -6,6 +6,7 @@ import {ConfigurationService} from './configuration.service';
 import {Http, Headers} from '@angular/http';
 import {AuthHeader} from './auth-header';
 import {Observable} from 'rxjs';
+import InvalidArgumentError = webdriver.error.InvalidArgumentError;
 
 @Injectable()
 export class RetrospectiveService {
@@ -35,37 +36,59 @@ export class RetrospectiveService {
           withCredentials: true,
           headers: AuthHeader.appendAuthHeader(new Headers(), jwtToken)
         }).map(response => {
-        console.log(response.toString());
-        return'';// return (<IBasicRetrospective<IUser>> response.json()).uuid;
+        return RetrospectiveService.extractRetrospectiveIdFromLocation(response.headers.get('Location'));
       }, e => {
         console.log(e);
       });
     });
   }
 
-  private getJwtToken(shortName: string): Observable<string> {
+  private getJwtToken(shortName?: string): Observable<string> {
     if (!this.authService.isUserLoggedIn()) {
+      if (shortName == null) {
+        throw new InvalidArgumentError('No User Logged in an there is no shortName for create new User');
+      }
       console.log('no User logged in register new user with shportname: ' + shortName);
       return this.userService.createUser(shortName).map(isSuccess => {
         return this.authService.getAuthenticationToken();
       });
     }
-
     return Observable.create((observer) => {
       observer.next(this.authService.getAuthenticationToken());
       observer.complete();
     });
   }
 
-  public getRetrospective(sessionKey: string): IBasicRetrospective<IUser> {
+  public getRetrospective(sessionKey: string): Observable<IBasicRetrospective<IUser>> {
     if (this._currentRetrospective != null && this._currentRetrospective.uuid === sessionKey) {
-      return this._currentRetrospective;
+      return Observable.create((observer) => {
+        observer.next(this._currentRetrospective);
+        observer.complete();
+      });
     }
-
-    return null;
+    return this.getJwtToken().flatMap(jwtToken => {
+      return this.http.get(this.configuration.retrospectiveEndpoint + '/' + sessionKey,
+        {
+          withCredentials: true,
+          headers: AuthHeader.appendAuthHeader(new Headers(), jwtToken)
+        }).map(response => {
+        this._currentRetrospective = response.json();
+        return this._currentRetrospective;
+      })
+    });
   }
 
   public get currentRetrospective(): IBasicRetrospective<IUser> {
     return this._currentRetrospective;
   }
+
+  public static extractRetrospectiveIdFromLocation(location: string): string {
+    if (location == null) {
+      return null;
+    }
+    let id: string = location.substring((location.lastIndexOf('/') + 1), location.length);
+    console.log('id for retrospective is: ' + id);
+    return id;
+  }
+
 }
