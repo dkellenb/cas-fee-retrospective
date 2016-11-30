4ef9328dde2976b36dffda768642e023ad9818a9
Daniel Kellenberger <daniel@kellenberger.li>
2016-11-30 13:04:45 +0100

    fix imports

--- client/src/app/core/retrospective/retrospective-topic-board/retrospective-topic-board.component.ts
+++ client/src/app/core/retrospective/retrospective-topic-board/retrospective-topic-board.component.ts
@@ -1,5 +1,7 @@
 import {Component, OnInit, Input} from '@angular/core';
-import {IBasicRetrospectiveTopic, IRetrospectiveUser} from '../../../../../../shared/src/model';
+import {IBasicRetrospectiveTopic} from '../../../shared/model/RetrospectiveDomainModel';
+import {IRetrospectiveUser} from '../../../shared/model/UserDomainModel';
+
 @Component({
   selector: 'rsb-retrospective-topic-board',
   templateUrl: './retrospective-topic-board.component.html',
