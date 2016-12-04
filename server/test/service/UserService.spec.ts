a5fd7e408bc0c479ab615f2f1238c8241f2d1e54
Daniel Kellenberger <daniel@kellenberger.li>
2016-12-04 10:35:25 +0100

    server: additional tests for UserService

--- server/test/service/UserService.spec.ts
+++ server/test/service/UserService.spec.ts
@@ -1,9 +1,12 @@
-import {UserService} from '../../src/service/UserService';
+import {UserService, UserUnknown} from '../../src/service/UserService';
 import {UserRepository} from '../../src/repository/UserRepository';
 import {UserJwtService} from '../../src/service/UserJwtService';
 import * as TypeMoq from 'typemoq';
 import {CreateUserJSON, UserRole} from '../../../client/src/app/shared/model/UserDomainModel';
 import { expect } from 'chai';
+import {IUserJwt} from '../../src/service/model/User';
+import {IPersistedUser} from '../../src/repository/model/UserDbModel';
+import { Request } from 'express';
 
 describe('UserService', function() {
 
@@ -40,6 +43,8 @@ describe('UserService', function() {
 
       // when
       let promise = userService.createUser(createUserJSON);
+
+      // then
       promise.then((user) => {
         expect(user.uuid).to.not.be.null;
         expect(user.name).to.be.equal(createUserJSON.name);
@@ -52,6 +57,91 @@ describe('UserService', function() {
       });
     });
 
+  });
+
+  describe('#getJwtUser()', function () {
+
+    let iUserJwt: IUserJwt;
+    let iPersistedUser: IPersistedUser;
+
+    beforeEach(function() {
+      iUserJwt = <IUserJwt>{
+        uuid: '123-123-123',
+        name: 'Mr. Bean',
+        shortName: 'MB',
+        validUntil: 123,
+        systemRole: UserRole.USER
+      };
+
+      iPersistedUser = <IPersistedUser>{};
+      iPersistedUser.uuid = iUserJwt.uuid;
+      iPersistedUser.name = iUserJwt.name;
+    });
+
+    let request = <Request>{};
+
+
+    it('Should return JWT-based user from request', function(done) {
+      // given
+      userJwtService.setup(s => s.getJwtUser(TypeMoq.It.isAny())).returns(() => iUserJwt);
+      userRepository.setup(r => r.findByUuid(TypeMoq.It.isAnyString(), TypeMoq.It.isAny())).callback((uuid, fncCallback) => {
+        fncCallback(null, iPersistedUser);
+      });
+
+      // when
+      let getJwtPromise = userService.getJwtUser(request);
+
+      // then
+      getJwtPromise.then((user) => {
+        expect(user.uuid).to.be.equal(iUserJwt.uuid);
+        done();
+      }).catch((err) => {
+        done(err);
+      });
+    });
+
+    it('Should return user unknown if db returns no data', function (done) {
+      // given
+      userJwtService.setup(s => s.getJwtUser(TypeMoq.It.isAny())).returns(() => iUserJwt);
+      userRepository.setup(r => r.findByUuid(TypeMoq.It.isAnyString(), TypeMoq.It.isAny())).callback((uuid, fncCallback) => {
+        fncCallback(null, null); // no data
+      });
+
+      // when
+      let getJwtPromise = userService.getJwtUser(request);
+
+      // then
+      getJwtPromise.then((user) => {
+        done('Should not return an user');
+      }).catch((err) => {
+        expect(err).to.be.instanceof(UserUnknown);
+      }).then(done, done);
+    });
+
+    it('Should return the error if a db error occurs', function(done) {
+      // given
+      userJwtService.setup(s => s.getJwtUser(TypeMoq.It.isAny())).returns(() => iUserJwt);
+      userRepository.setup(r => r.findByUuid(TypeMoq.It.isAnyString(), TypeMoq.It.isAny())).callback((uuid, fncCallback) => {
+        fncCallback('err', null); // no data
+      });
+
+      // when
+      let getJwtPromise = userService.getJwtUser(request);
+
+      // then
+      getJwtPromise.then((user) => {
+        done('Should not return an user');
+      }).catch((err) => {
+        expect(err).to.be.equals('err');
+      }).then(done, done);
+    });
+
+  });
+
+  describe('#getJwt', function () {
+    it('Should return the JWT for a specific user', function(done) {
+
+    });
   });
 
 });
