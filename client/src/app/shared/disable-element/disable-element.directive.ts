38b1cbd49da81bfbf852acb8e9122d674f65652b
bruede <benj.ruede@gmail.com>
2016-12-05 15:01:08 +0100

    - Some Button Icons changed to FontAwesome - Disable Element now disable just disable elements and set on rest the no_select class

--- client/src/app/shared/disable-element/disable-element.directive.ts
+++ client/src/app/shared/disable-element/disable-element.directive.ts
@@ -1,4 +1,12 @@
-import {Directive, Renderer, ElementRef, OnDestroy, Inject, OnInit, ReflectiveInjector, ResolvedReflectiveProvider, Optional} from '@angular/core';
+import {
+  Directive,
+  Renderer,
+  ElementRef,
+  OnDestroy,
+  OnInit,
+  Optional,
+  ViewContainerRef
+} from '@angular/core';
 import {DisableService} from './disable.service';
 import {Subscription} from 'rxjs';
 
@@ -11,20 +19,41 @@ import {Subscription} from 'rxjs';
 export class DisableElementDirective implements OnDestroy, OnInit {
   private _subscrition: Subscription;
 
-  constructor(private el: ElementRef, private renderer: Renderer, @Optional() private disableService: DisableService) {
+
+  private disableElements: string[] = ['BUTTON', 'INPUT', 'SELECT', 'TEXTAREA', 'OPTGROUP', 'OPTION', 'FIELDSET'];
+  private disable;
+
+  constructor(private el: ElementRef, private renderer: Renderer,
+              @Optional() private disableService: DisableService) {
+    this.disable = this.disableElements.indexOf(this.el.nativeElement.tagName.toUpperCase()) > -1;
   }
 
   ngOnInit(): void {
     if (this.disableService != null) {
-      this._subscrition = this.disableService.hasBeenDisabled$.subscribe(this.setDisabled.bind(this));
+      this._subscrition = this.disableService.hasBeenDisabled$.subscribe((value: boolean) => {
+        if (this.disable) {
+          this.setDisabled(value);
+        } else {
+          this.setNoSelectionClass(value);
+        }
+      });
+    }
+  }
+
+
+  private setNoSelectionClass(value: boolean) {
+    if (value) {
+      this.renderer.setElementClass(this.el.nativeElement, 'no_select', true);
+    } else {
+      this.renderer.setElementClass(this.el.nativeElement, 'no_select', false);
     }
   }
 
   private setDisabled(value: boolean) {
     if (value) {
-      this.el.nativeElement.setAttribute('disabled', '');
+      this.renderer.setElementAttribute(this.el.nativeElement, 'disabled', '');
     } else {
-      this.el.nativeElement.removeAttribute('disabled');
+      this.renderer.setElementAttribute(this.el.nativeElement, 'disabled', null);
     }
   }
 
