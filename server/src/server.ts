1373df2211b76d51182e6a8b557cce1d91b732db
Daniel Kellenberger <daniel@kellenberger.li>
2016-12-07 21:39:01 +0100

    add security headers (as discussed in our lecture)

--- server/src/server.ts
+++ server/src/server.ts
@@ -116,8 +116,21 @@ class RetroServer {
         // to the API (e.g. in case you use sessions)
         res.setHeader('Access-Control-Allow-Credentials', 'true');
 
-        // Pass to next layer of middleware
-        next();
+        // Strict transport security
+        if (process.env.STRICT_TRANSPORT_SECURITY) {
+          res.setHeader('Strict-Transport-Security', 'max-age=16070400;');
+        }
+
+        // Frames not allowed
+        res.setHeader('X-Frame-Options', 'deny');
+
+        // intercept OPTIONS method
+        if (req.method === 'OPTIONS') {
+          res.sendStatus(200);
+        } else {
+          // Pass to next layer of middleware
+          next();
+        }
       });
 
       app.use(bodyParser.urlencoded({
