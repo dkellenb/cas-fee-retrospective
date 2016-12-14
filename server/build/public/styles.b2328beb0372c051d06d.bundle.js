a7241ddfd9a9febc1b954068266c6f777a57b578
Daniel Kellenberger <daniel@kellenberger.li>
2016-12-14 10:47:17 +0100

    heroku-build-0.24

--- server/build/public/styles.b2328beb0372c051d06d.bundle.js
+++ server/build/public/styles.b2328beb0372c051d06d.bundle.js
@@ -1,2 +1,2 @@
-webpackJsonp([2,3],{1132:function(n,o,c){n.exports=c(484)},484:function(n,o){}},[1132]);
+webpackJsonp([2,3],{1138:function(n,o,c){n.exports=c(485)},485:function(n,o){}},[1138]);
 //# sourceMappingURL=styles.b2328beb0372c051d06d.bundle.map
