ed81954a2db34f5ab5391c89cd1bab19b5729a9e
bruede <benj.ruede@gmail.com>
2016-12-18 19:56:08 +0100

    - Add Tests for getComment / createComment

--- client/src/app/core/services/retrospective.service.spec.ts
+++ client/src/app/core/services/retrospective.service.spec.ts
@@ -10,25 +10,37 @@ import {AuthHttp} from 'angular2-jwt';
 import {StubUserService} from './user.service.spec';
 import {WebSocketService} from './web-socket.service';
 import {MockWebSocketService} from './web-socket.service.spec';
+import {CreateCommentJSON} from '../../shared/model/retrospective/CreateCommentJSON';
+import {IBasicRetrospectiveComment} from '../../shared/model/retrospective/IBasicRetrospectiveComment';
+
+const initialRetro: IBasicRetrospective<IRetrospectiveUser> = {
+  'uuid': '56e03af4-8b6c-4498-a14b-f8d87cf0dfeb',
+  'name': 'Initial Retro',
+  'description': 'This is a Retro like direct from the serve after creation',
+  'status': 'OPEN',
+  'attendees': [{'uuid': 'ea3ad9cc-1444-4153-93c4-a9ef72d37d98', 'name': '', 'shortName': 'test', 'role': 'MANAGER'}],
+  'topics': [{'uuid': '11546e54-d2cb-4451-b739-bc69f175ab2a', 'name': 'Stop doing', 'comments': []}, {
+    'uuid': '600bd703-ec32-4d3f-ad35-96216e999db3',
+    'name': 'Continue doing',
+    'comments': []
+  }, {'uuid': '2c8ae4a5-cbae-4c49-9c2d-e0834277e711', 'name': 'Start doing', 'comments': []}]
+};
+
+const commentFromServer: IBasicRetrospectiveComment<IRetrospectiveUser> = {
+  'uuid': '6830afdd-22cb-4171-b611-b975081e58ef',
+  'title': 'Comment Title',
+  'description': 'Here is a Description',
+  'anonymous': true,
+  'author': {'uuid': 'ea3ad9cc-1444-4153-93c4-a9ef72d37d98', 'name': '', 'shortName': 'test', 'role': 'MANAGER'},
+  'votes': []
+};
+
+const jwtUserToken: string = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1dWlkIjoiZWEzYWQ5Y2MtMTQ0NC00MT' +
+  'UzLTkzYzQtYTllZjcyZDM3ZDk4IiwidmFsaWRVbnRpbCI6MTU3NjI2NDIxMywic2hvcnROYW1lIjoidGVzdCIsIm5hbWUiOm51' +
+  'bGwsImVtYWlsIjpudWxsLCJzeXN0ZW1Sb2xlIjoiVVNFUiIsImlhdCI6MTQ4MTY1NjIxNH0.wPS3jruXDXgwd8hlKKV0X60A-nmID8pl8n4z-19Raok';
+
 
 describe('Service: Retrospective', () => {
-
-
-  const initialRetro: IBasicRetrospective<IRetrospectiveUser> = {
-    'uuid': '56e03af4-8b6c-4498-a14b-f8d87cf0dfeb',
-    'name': 'Initial Retro',
-    'description': 'This is a Retro like direct from the serve after creation',
-    'status': 'OPEN',
-    'attendees': [{'uuid': 'ea3ad9cc-1444-4153-93c4-a9ef72d37d98', 'name': '', 'shortName': 'test', 'role': 'MANAGER'}],
-    'topics': [{'uuid': '11546e54-d2cb-4451-b739-bc69f175ab2a', 'name': 'Stop doing', 'comments': []}, {
-      'uuid': '600bd703-ec32-4d3f-ad35-96216e999db3',
-      'name': 'Continue doing',
-      'comments': []
-    }, {'uuid': '2c8ae4a5-cbae-4c49-9c2d-e0834277e711', 'name': 'Start doing', 'comments': []}]
-  };
-  const jwtUserToken: string = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1dWlkIjoiZWEzYWQ5Y2MtMTQ0NC00MT' +
-    'UzLTkzYzQtYTllZjcyZDM3ZDk4IiwidmFsaWRVbnRpbCI6MTU3NjI2NDIxMywic2hvcnROYW1lIjoidGVzdCIsIm5hbWUiOm51' +
-    'bGwsImVtYWlsIjpudWxsLCJzeXN0ZW1Sb2xlIjoiVVNFUiIsImlhdCI6MTQ4MTY1NjIxNH0.wPS3jruXDXgwd8hlKKV0X60A-nmID8pl8n4z-19Raok';
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -56,6 +68,13 @@ describe('Service: Retrospective', () => {
         }
       ]
     });
+
+    TestBed.get(MockWebSocketService).expectedRetroId = '56e03af4-8b6c-4498-a14b-f8d87cf0dfeb';
+    TestBed.get(AuthenticationService).setAuthenticationToken(jwtUserToken);
+  });
+
+  afterEach(() => {
+    TestBed.get(AuthenticationService).logoutUser();
   });
 
   it('should be able to inject all parts', inject([RetrospectiveService], (service: RetrospectiveService) => {
@@ -103,11 +122,10 @@ describe('Service: Retrospective', () => {
     })));
 
   it('should create Retrospective', async(inject(
-    [RetrospectiveService, MockBackend, AuthenticationService],
-    (service: RetrospectiveService, backend: MockBackend, authService: AuthenticationService) => {
+    [RetrospectiveService, MockBackend],
+    (service: RetrospectiveService, backend: MockBackend) => {
 
       // SetUp
-      authService.setAuthenticationToken(jwtUserToken);
       backend.connections.subscribe((c: MockConnection) => {
         expect(c.request.url).toEqual('http://localhost:3000/rest/retrospectives');
         expect(c.request.method).toBe(RequestMethod.Post);
@@ -116,17 +134,122 @@ describe('Service: Retrospective', () => {
         expect(body.description).toEqual('Description');
 
         let headers: Headers = new Headers();
-        headers.append('Location', '123');
-        c.mockRespond(new Response(new ResponseOptions({headers: headers, body: 'create'})));
+        headers.append('Location', 'http://localhost:3000/rest/retrospectives/123');
+        c.mockRespond(new Response(new ResponseOptions({headers: headers, body: 'create', status: 201})));
       });
 
       // Execute
-      service.createRetrospective('Title', 'Description').subscribe((retro: string) => {
-        console.log(retro);
+      service.createRetrospective('Title', 'Description').subscribe((retroId: string) => {
+        expect(retroId).toEqual('123');
+      });
+    })));
+
+  it('should create Retrospective', async(inject(
+    [RetrospectiveService, MockBackend],
+    (service: RetrospectiveService, backend: MockBackend) => {
+
+      // SetUp
+      backend.connections.subscribe((c: MockConnection) => {
+        expect(c.request.url).toEqual('http://localhost:3000/rest/retrospectives');
+        expect(c.request.method).toBe(RequestMethod.Post);
+        let body = JSON.parse(c.request.getBody());
+        expect(body.name).toEqual('Title');
+        expect(body.description).toEqual('Description');
+
+        let headers: Headers = new Headers();
+        headers.append('Location', 'http://localhost:3000/rest/retrospectives/123');
+        c.mockRespond(new Response(new ResponseOptions({headers: headers, body: 'create', status: 201})));
       });
 
-      // TearDown
-      authService.logoutUser();
+      // Execute
+      service.createRetrospective('Title', 'Description').subscribe((retroId: string) => {
+        expect(retroId).toEqual('123');
+      });
+    })));
+
+  it('should get Comment', async(inject(
+    [RetrospectiveService, MockBackend],
+    (service: RetrospectiveService, backend: MockBackend) => {
+
+      // SetUp
+      let comment: CreateCommentJSON = <CreateCommentJSON>{};
+      comment.title = 'Comment Title';
+      comment.description = 'Here is a Description';
+      comment.anonymous = false;
+
+      backend.connections.subscribe((c: MockConnection) => {
+
+        // Load initial Retro in Service
+        if (c.request.url === 'http://localhost:3000/rest/retrospectives/56e03af4-8b6c-4498-a14b-f8d87cf0dfeb') {
+          c.mockRespond(new Response(new ResponseOptions({body: initialRetro, status: 200})));
+          return;
+        }
+
+        expect(c.request.url).toEqual('http://localhost:3000/rest/retrospectives/' +
+          '56e03af4-8b6c-4498-a14b-f8d87cf0dfeb/comments/6830afdd-22cb-4171-b611-b975081e58ef');
+        expect(c.request.method).toBe(RequestMethod.Get);
+        c.mockRespond(new Response(new ResponseOptions({body: commentFromServer, status: 200})));
+      });
+
+      // Execute
+      service.getRetrospective('56e03af4-8b6c-4498-a14b-f8d87cf0dfeb')
+        .subscribe(() => {
+          service.getComment('6830afdd-22cb-4171-b611-b975081e58ef')
+            .subscribe((returnComment: IBasicRetrospectiveComment<IRetrospectiveUser>) => {
+              expect(returnComment).toEqual(commentFromServer);
+            });
+        });
+
+    })));
+
+  it('should create Comment', async(inject(
+    [RetrospectiveService, MockBackend],
+    (service: RetrospectiveService, backend: MockBackend) => {
+
+      // SetUp
+      let comment: CreateCommentJSON = <CreateCommentJSON>{};
+      comment.title = 'Comment Title';
+      comment.description = 'Here is a Description';
+      comment.anonymous = false;
+
+      backend.connections.subscribe((c: MockConnection) => {
+
+        // Load initial Retro in Service
+        if (c.request.url === 'http://localhost:3000/rest/retrospectives/56e03af4-8b6c-4498-a14b-f8d87cf0dfeb') {
+          c.mockRespond(new Response(new ResponseOptions({body: initialRetro, status: 200})));
+          return;
+        }
+
+        if (c.request.url === 'http://localhost:3000/rest/retrospectives/' +
+          '56e03af4-8b6c-4498-a14b-f8d87cf0dfeb/comments/6830afdd-22cb-4171-b611-b975081e58ef') {
+          c.mockRespond(new Response(new ResponseOptions({body: commentFromServer, status: 200})));
+          return;
+        }
+
+        expect(c.request.url).toEqual('http://localhost:3000/rest/retrospectives/' +
+          '56e03af4-8b6c-4498-a14b-f8d87cf0dfeb/topics/11546e54-d2cb-4451-b739-bc69f175ab2a/comments');
+        expect(c.request.method).toBe(RequestMethod.Post);
+        let body = JSON.parse(c.request.getBody());
+        expect(body.title).toEqual(comment.title);
+        expect(body.description).toEqual(comment.description);
+        expect(body.anonymous).toEqual(comment.anonymous);
+
+        let headers: Headers = new Headers();
+        headers.append('Location', 'http://localhost:3000/rest/retrospectives/' +
+          '56e03af4-8b6c-4498-a14b-f8d87cf0dfeb/comments/6830afdd-22cb-4171-b611-b975081e58ef');
+        c.mockRespond(new Response(new ResponseOptions({headers: headers, body: commentFromServer, status: 201})));
+      });
+
+      // Execute
+      service.getRetrospective('56e03af4-8b6c-4498-a14b-f8d87cf0dfeb')
+        .subscribe(() => {
+          service.createComment('11546e54-d2cb-4451-b739-bc69f175ab2a', comment)
+            .subscribe((returnComment: IBasicRetrospectiveComment<IRetrospectiveUser>) => {
+              expect(returnComment).toEqual(commentFromServer);
+              console.log(returnComment);
+            });
+        });
+
     })));
 });
 
